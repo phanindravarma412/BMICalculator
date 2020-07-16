cc1ca483bc05b9aa36a510028fd260aa4be8a62c
phanindravarma412 <phanindravarma412@gmail.com>
2020-07-16 14:23:21 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/phani/bmi_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.phani.bmi_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/results_page.dart
@@ -0,0 +1,74 @@
+import 'package:bmi_calculator/components/bottom_button.dart';
+import 'package:bmi_calculator/constants.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../components/reusable_card.dart';
+
+class ResultsPage extends StatelessWidget {
+  ResultsPage(
+      {@required this.bmiResult,
+      @required this.resultText,
+      @required this.interpretation});
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('BMI Calculator'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Your Result',
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ResuableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Center(
+                    child: Text(
+                      resultText.toUpperCase(),
+                      style: kResultTextStyle,
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      bmiResult.toUpperCase(),
+                      style: kBMITextStyle,
+                    ),
+                  ),
+                  Text(
+                    interpretation,
+                    textAlign: TextAlign.center,
+                    style: kBodyTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+            buttonTitle: 'RE-CALCULATE',
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
